66e6b5a81e812ce76532b7191b10df2794cb4d4f
Phien Pham <phiendpham@gmail.com>
2019-05-28 17:31:29 +0700

    Implement Blockchain problem

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,7 +1,70 @@
+import datetime
+import hashlib
+
+
+class Node:
+
+    def __init__(self, block, previous=None):
+        self.block = block
+        self.previous = previous
+
+
+class BlockChain:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_block(self, data):
+        previous_hash = None
+        if self.tail:
+            previous_hash = self.tail.block.hash
+
+        block = Block(data, previous_hash)
+        node = Node(block,  self.tail)
+
+        if self.head == None:
+            self.head = node
+        else:
+            block.previous = self.tail
+            block.previous_hash = self.tail.block.hash
+
+        self.tail = node
+
+
 class Block:
 
-    def __init__(self, timestamp, data, previous_hash):
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
+    def __init__(self, data, previous_hash):
+        self.timestamp = datetime.datetime.utcnow()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash(self.data)
+
+    def calc_hash(self, data):
+        sha = hashlib.sha256()
+        hash_str = data.encode('utf-8')
+        sha.update(hash_str)
+        return sha.hexdigest()
+
+
+def test_blockchain():
+    chain = BlockChain()
+
+    chain.add_block("Sample test data")
+    assert chain.tail == chain.head
+    assert chain.tail.block.previous_hash == None
+    first_block_hash = chain.tail.block.hash
+
+    chain.add_block("Add new data")
+    assert chain.tail != chain.head
+    assert chain.tail.block.previous_hash != None
+    second_block_hash = chain.tail.block.hash
+    second_block_prev_hash = chain.tail.block.previous_hash
+    assert first_block_hash == second_block_prev_hash
+    assert second_block_hash != second_block_prev_hash
+
+    print("Tests passed!")
+
+
+if __name__ == "__main__":
+    test_blockchain()
