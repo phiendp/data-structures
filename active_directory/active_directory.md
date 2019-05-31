1dbfe13d3791466ab796713d049aaa5238f1d8f3
Phien Pham <phiendpham@gmail.com>
2019-05-31 11:08:28 +0700

    Implement Active Directory problem

--- active_directory/active_directory.md
+++
@@ -1,2 +0,0 @@
-# Active Directory
-In Windows Active Directory, a group can consist of user(s) and group(s) themselves. We can construct this hierarchy as such. Where User is represented by str representing their ids.
--- active_directory/active_directory.py
+++ active_directory/active_directory.py
@@ -28,4 +28,38 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
-    return None
+    if user is None or group is None:
+        return False
+
+    if user in group.users:
+        return True
+
+    for group in group.groups:
+        return is_user_in_group(user, group)
+    return False
+
+
+def active_directory_specs():
+    parent = Group("parent")
+    child = Group("child")
+    sub_child = Group("subchild")
+
+    sub_child_user = "sub_child_user"
+    parent_user = "superuser"
+    sub_child.add_user(sub_child_user)
+
+    child.add_group(sub_child)
+    parent.add_group(child)
+    parent.add_user(parent_user)
+
+    assert is_user_in_group(parent_user, None) == False, "None group should return false"
+    assert is_user_in_group(None, parent) == False, "None user should return false"
+    assert is_user_in_group(sub_child_user, parent), "User not found in subgroup!"
+    assert is_user_in_group(sub_child_user, sub_child), "User not found in same group!"
+    assert is_user_in_group(parent_user, sub_child) == False, "User is only in super group!"
+
+    print("Tests passed!")
+
+
+if __name__ == "__main__":
+    active_directory_specs()
